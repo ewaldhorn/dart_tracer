27087eb1bb2797939d87b5dad6438aada2ad3aab
Ewald Horn <ewaldhorn@gmail.com>
2019-01-22 10:26:24 +0200

    Updated README Added a Dart program that produces a blank PNG and opens in with Preview on Mac.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # dart_tracer
-Simple Dart raytracer.  Heavily based on the awesome work at https://github.com/ssloy/tinyraytracer
+
+Simple Dart raytracer.  Heavily based on the awesome work known as [TinyRayTracer](https://github.com/ssloy/tinyraytracer).
+
+I followed the excellent tutorial by [ssloy](https://github.com/ssloy/tinyraytracer/wiki) to refresh my knowledge of ray tracing.
---
+++ blankfile.dart
@@ -0,0 +1,20 @@
+import 'dart:io' as IO;
+import 'package:image/image.dart';
+
+/**
+ * Create a blank image.
+ * Save it as a PNG.
+ * Open it via the default Mac app.
+ */
+
+void main() {
+  print("Started");
+  Image image = Image(1024, 768);
+
+  fill(image, getColor(255, 255, 255));
+
+  List<int> png = encodePng(image);
+  new IO.File('basic.png').writeAsBytesSync(png);
+
+  IO.Process.run('open', ['-a','Preview', 'basic.png']);
+}
