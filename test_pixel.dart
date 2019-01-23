1378c35e02048ed46c13348e03d32c61582848ee
Ewald Horn <ewaldhorn@gmail.com>
2019-01-23 16:25:47 +0200

    Added another test.

--- test_pixel.dart
+++ test_pixel.dart
@@ -9,7 +9,12 @@ void main() {
       expect(p.g, equals(0.2));
       expect(p.b, equals(0.3));
     });
-
+    test("Can create a zero pixel.", () {
+      Pixel p = Pixel.zero();
+      expect(p.r, equals(0.0));
+      expect(p.g, equals(0.0));
+      expect(p.b, equals(0.0));
+    });
     test("Can add positive to a pixel.", () {
       Pixel p = Pixel.zero();
       p.add(0.1, 0.11, 0.111);
